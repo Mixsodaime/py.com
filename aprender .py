b6bad46599644a8c2239bc6093d5d60a9773391d
Mixsodaime <114780589+Mixsodaime@users.noreply.github.com>
2022-11-08 19:42:44 -0300

    Create aprender .py

---
+++ aprender .py
@@ -0,0 +1,39 @@
+#faça funcao que calcule a area do trapezio dados
+
+a)base maior
+b)base menor
+c) altura
+
+lembrandoque aarea ser calculado por A=(B+b).h/2
+oprograma principal deve pedir os valores e usar afuncao
+paracalclar a area.
+
+
+
+baseMaior = input('valor de uma base: ')
+baseMenor = input('valor de outra base: ')
+altura    = input('valor de altura: ')
+area = (baseMaior + baseMenor) * altura / 2
+print('Area: ' , area
+
+2 trabalho :
+
+faça um programa em python com uma funcao chamada soma_imposto.A funcao
+possui dois parametros formais:
+
+a) taxa_imposto, que e a quantia de imposto sobre vendas expressa em porcentager
+
+b) custo, que e o custo de um item antes do imposto. A funcao "altera" o valor de custo para incluir o imposto sobre vendas.
+o programa principal deve pedir os dados e sar a funcao para calcular preco de produto.
+
+def somaimposto(taxaimposto,custo):
+    return(1 + taxaimposo/100) *custo
+    t = float(input('digite a taxa de imposto: '))
+    c = float(input('digite o custo:  '))
+    print('valor com imposto:' , somaImposto(t,c))
+    
+    
+    
+
+
+
